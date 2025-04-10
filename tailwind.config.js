74ede5ea43e9f9b5f63dd839321ab7c16c604410
DuocAnh <duocanh14@gmail.com>
2025-04-10 13:01:22 +0700

    install tailwind shadcn

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+import { defineConfig } from 'tailwindcss'
+
+export default defineConfig({
+  darkMode: 'class', // Kích hoạt chế độ dark mode sử dụng class "dark"
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}' // Quét các file trong thư mục src để tìm class Tailwind
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // Định nghĩa các màu sắc tùy chỉnh
+        background: 'var(--color-background)',
+        foreground: 'var(--color-foreground)',
+        primary: 'var(--color-primary)',
+        'primary-foreground': 'var(--color-primary-foreground)',
+        secondary: 'var(--color-secondary)',
+        'secondary-foreground': 'var(--color-secondary-foreground)',
+        muted: 'var(--color-muted)',
+        'muted-foreground': 'var(--color-muted-foreground)',
+        accent: 'var(--color-accent)',
+        'accent-foreground': 'var(--color-accent-foreground)',
+        destructive: 'var(--color-destructive)',
+        border: 'var(--color-border)',
+        input: 'var(--color-input)',
+        ring: 'var(--color-ring)',
+        sidebar: 'var(--color-sidebar)',
+        'sidebar-foreground': 'var(--color-sidebar-foreground)'
+      },
+      borderRadius: {
+        sm: 'var(--radius-sm)',
+        md: 'var(--radius-md)',
+        lg: 'var(--radius-lg)',
+        xl: 'var(--radius-xl)'
+      }
+    }
+  },
+  plugins: []
+})
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), tailwindcss()],
   resolve: {
-    alias: [{ find: '~', replacement: '/src' }]
+    alias: {
+      // eslint-disable-next-line no-undef
+      '@': path.resolve(__dirname, './src')
+    }
   }
 })
