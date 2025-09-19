3eee80f5400436fa116ecd676656506dddd151ee
ducAnhHanu67 <ducanhhanu2020@gmail.com>
2025-09-19 11:53:02 +0700

    update phần màu và chữ time

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,4 @@
-// export const API_ROOT = 'http://localhost:3000'
+export const API_ROOT = 'http://localhost:3000'
 // export const API_ROOT = 'https://roboticsworld1.com'
 // export const API_ROOT = 'http://103.179.190.146:3000'
 export const API_ROOT = 'https://api.dreamaura.online'
